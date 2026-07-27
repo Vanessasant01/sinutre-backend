a58c69380d69b8fe6b30233937aaaabf0f147fcc
Vanessa dos Santos Fontenele <Vanessasant4444@gmail.com>
2026-07-26 21:59:10 -0300

    ultimo ajuste final

--- dist/index.js
+++ dist/index.js
@@ -2,11 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
 const prisma_1 = require("./prisma");
+// OBRIGA usar a porta que o Railway mandar, ou 3333 localmente
 const PORT = process.env.PORT || 3333;
 async function startServer() {
     await prisma_1.prisma.$connect();
     app_1.app.listen(PORT, () => {
-        console.log(`SiNutre back rodando em http://localhost:${PORT}`);
+        console.log(`✅ SiNutre Backend rodando na porta ${PORT}`);
     });
 }
 startServer();
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import { app } from './app'
 import { prisma } from './prisma'
 
+// OBRIGA usar a porta que o Railway mandar, ou 3333 localmente
 const PORT = process.env.PORT || 3333
 
 async function startServer() {
   await prisma.$connect()
-  
   app.listen(PORT, () => {
-    console.log(`SiNutre back rodando em http://localhost:${PORT}`)
+    console.log(`✅ SiNutre Backend rodando na porta ${PORT}`)
   })
 }
 
