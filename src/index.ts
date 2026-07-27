9baa44b72baaa7246e1044b0eaeae9981f1e0fb7
Vanessa dos Santos Fontenele <Vanessasant4444@gmail.com>
2026-07-27 01:24:16 -0300

    corrige build e remove dist antiga

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
-import { debugPort } from "process";
-import { app } from "./app";
-import { prisma } from "./prisma";
-
 const PORT = process.env.PORT || 3333;
 
 async function startServer() {
   await prisma.$connect();
-  app.listen(debugPort, '0.0.0.0', () => {
-    console.log(`SiNutre Backend rodando na porta ${debugPort}`);
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`SiNutre Backend rodando na porta ${PORT}`);
   });
 }
 startServer();
